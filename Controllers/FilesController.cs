6180cbf9d287e017a42ff986d53c7bb2fe7b1cd0
Can <ccevi003@fiu.edu>
2020-07-05 12:32:42 -0400

    Inital Commit
    initial commit

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FileUpload.Models;
+
+namespace FileUpload.Controllers
+{
+    public class FilesController : Controller
+    {
+        private fileUploadEntities3 db = new fileUploadEntities3();
+
+        // GET: Files
+        public ActionResult Index()
+        {
+            var f = db.Files.ToList();
+            f.Reverse();
+            return View(f);
+        }
+
+        // GET: Files/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        public ActionResult FileDetails(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var file = db.Files.Where(x => x.FileId == id).FirstOrDefault();
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+
+        /*
+        public FileResult Download(int? id)
+        {
+            var fileToRetrieve = db.Files.Find(id);
+            return File(fileToRetrieve.FileContent, "application/octet-stream", fileToRetrieve.FileName + fileToRetrieve.FileExt);
+        }*/
+
+        public FileResult Download(string id)
+        {
+            var fileToRetrieve = db.Files.Where(x => x.FileId == id).FirstOrDefault();
+            return File(fileToRetrieve.FileContent, "application/octet-stream", fileToRetrieve.FileName + fileToRetrieve.FileExt);
+        }
+
+
+
+        public ActionResult MyFiles()
+        {
+            var tempSess = (int)Session["Id"];
+            
+            return View(db.Files.Where(x => x.UserId == tempSess));
+        }
+
+        public ActionResult Search(string fname, string fext, string fdesc)
+        {
+            
+            return View("Index",db.Files.Where(x => x.FileName.Contains(fname) && x.FileExt.Contains(fext) && x.FileDesc.Contains(fdesc)));
+        }
+
+        // GET: Files/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Files/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserId,FileName,FileContent,FileId,FileExt,FileDesc,UploadDate")] File file, HttpPostedFileBase uFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if(uFile != null)
+                {
+                    file.UserId = (int) Session["Id"];
+                    file.FileContent = new byte[uFile.ContentLength];
+                    uFile.InputStream.Read(file.FileContent, 0, uFile.ContentLength);
+                    file.FileExt = MimeTypes.MimeTypeMap.GetExtension(uFile.ContentType);
+                    string hexString = "";
+                    Random rnd = new Random();
+                    char[] hexChar = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', };
+                    for(var i = 0; i< 20; i++)
+                    {
+                        
+                        int j = rnd.Next(0, 16);
+                        hexString += hexChar[j];
+                    }
+                    //var random = new Random();
+                    //var hex = String.Format("{0:X20}", random.Next(0x1000000));
+                    file.FileId = hexString;
+                }
+                db.Files.Add(file);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(file);
+        }
+
+        // GET: Files/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: Files/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserId,FileName,FileContent,FileId,FileExt,FileDesc,UploadDate")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(file).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(file);
+        }
+
+        // GET: Files/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: Files/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            File file = db.Files.Find(id);
+            db.Files.Remove(file);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using FileUpload.Models;
+
+namespace FileUpload.Controllers
+{
+    public class UsersController : Controller
+    {
+        private fileUploadEntities2 db = new fileUploadEntities2();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FileUpload.Models.User user)
+        {
+            var userDetails = db.Users.Where(x => x.Username == user.Username && x.Password == user.Password).FirstOrDefault();
+            if (userDetails == null)
+            {
+                //user.loginError = "Wrong User Name or Password";
+                return View();
+            }
+            else
+            {
+                Session["Id"] = userDetails.Id;
+                Session["UserName"] = userDetails.Username;
+                return RedirectToAction("Profile", "Users");
+            }
+        }
+        public ActionResult LogOut()
+        {
+            int userID = (int)Session["Id"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+
+
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Username,Email,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var uNameTaken = db.Users.Any(x => x.Username == user.Username || x.Email == user.Email);
+                if (uNameTaken)
+                {
+                    ViewBag.err = "Username/email already taken!";
+                    return View();
+                }
+                
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Username,Email,Password")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
